b15a5cd7ac293f3fcd11b25ca55a8ef61c158cca
PedroRojasF <pedro.rojas.f24@gmail.com>
2025-12-05 20:10:19 -0500

    center
    Co-authored-by: Don David <dadch1404@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
             At Pragmatic, we make software that works. We believe that software
             should be reliable, efficient, and easy to use.
           </h1>
-          <div style={{ marginTop: ".75rem" }}>
+          <div style={{ marginTop: "1.15rem" }}>
             <SocialLink href="https://github.com/empiricalhq/">
               GITHUB
             </SocialLink>
@@ -18,36 +18,34 @@ function App() {
         </div>
 
         <div className="projects-grid">
-          <div>
-            <ProjectCard
-              color="blue"
-              icon="🥀"
-              title="Gaaaaa aaaaaaa"
-              appName="WhatsApp Bot"
-              year="2025"
-            />
-            <ProjectCard
-              color="white"
-              icon="🕊️"
-              title="A monorepo"
-              appName="Lima Limpia"
-              year="2025"
-            />
-            <ProjectCard
-              color="yellow"
-              icon="☠️"
-              title="A monorepo"
-              appName="Lima Limpia"
-              year="2025"
-            />
-            <ProjectCard
-              color="red"
-              icon="😭"
-              title="A simple app to download files"
-              appName="downloader"
-              year="2025"
-            />
-          </div>
+          <ProjectCard
+            color="blue"
+            icon="🥀"
+            title="Gaaaaa aaaaaaa"
+            appName="WhatsApp Bot"
+            year="2025"
+          />
+          <ProjectCard
+            color="white"
+            icon="🕊️"
+            title="A monorepo"
+            appName="Lima Limpia"
+            year="2025"
+          />
+          <ProjectCard
+            color="yellow"
+            icon="☠️"
+            title="A monorepo"
+            appName="Lima Limpia"
+            year="2025"
+          />
+          <ProjectCard
+            color="red"
+            icon="😭"
+            title="A simple app to download files"
+            appName="downloader"
+            year="2025"
+          />
         </div>
       </div>
     </div>
